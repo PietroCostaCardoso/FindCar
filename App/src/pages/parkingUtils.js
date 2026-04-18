a04dfd892e7eb66f4b21c74a91fd5c96a3cf830d
Pietro Costa Cardoso <pietrocosta.cardoso1@gmail.com>
2026-04-18 10:39:14 -0300

    Correct parameter type in calculateParkingDetails
    Fix parameter type annotation for hourlyRate.

--- App/src/pages/parkingUtils.js
+++ App/src/pages/parkingUtils.js
@@ -2,7 +2,7 @@
  * Calculates parking duration and cost.
  * @param {string | Date} startTime 
  * @param {string | Date} endTime 
- * @param {number | s
+ * @param {number 
  * @returns {{duration: string, total: number}}
  */
 export const calculateParkingDetails = (startTime, endTime, hourlyRate) => {
