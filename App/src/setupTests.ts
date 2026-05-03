a28629f880cbe61ad00080a568b8f68d9be2fa84
Pietro Costa Cardoso <pietrocosta.cardoso1@gmail.com>
2026-05-03 15:18:55 -0300

    Update jest-dom documentation link

--- App/src/setupTests.ts
+++ App/src/setupTests.ts
@@ -1,7 +1,7 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/
+// learn more: https://github.com/testing
 import '@testing-library/jest-dom/extend-expect';
 
 // Mock matchmedia
