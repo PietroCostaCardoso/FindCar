a152ed66d4724c70a7719408de6656acabeac043
Pietro Costa Cardoso <pietrocosta.cardoso1@gmail.com>
2026-05-05 17:34:49 -0300

    Update setupTests.ts

--- App/src/setupTests.ts
+++ App/src/setupTests.ts
@@ -1,4 +1,4 @@
-// jest-dom adds custom jest matchers for 
+// jest-dom adds custom jest matchers 
 import '@testing-library/jest-dom/extend-expect';
 
 // Mock matchmedia-  ferramenta de teste que simula a API window.matchMedia do navegador em ambientes Node.js 
